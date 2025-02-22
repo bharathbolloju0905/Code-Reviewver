13b8563d3598eec8cbaaebb987eab7f666bcdca4
Bharath Bolloju <22tq1a6659@siddhartha.co.in>
2025-02-22 20:15:07 +0530

    "commited"

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,21 +2,21 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import ReviewerCode from './component/pages/ReviewerCode';
 import Home from './component/pages/Home';
-import SingUp from './component/Singup/Singup';
+import SignUp from './component/Singup/Singup';
 import { getUserContext } from './context/userContext';
 import SingIn from './component/SignIn/Signin';
 import Contact from './component/pages/Contact';
-import SignUp from './component/Singup/Singup';
+
 function App() {
   const { authUser} = getUserContext();
  
   return (
     <Routes>
       <Route path="/" element={ <Home />} />
-      <Route path="/review" element={ authUser? <ReviewerCode /> :<SingUp/>} />
+      <Route path="/review" element={ authUser? <ReviewerCode /> :<SignUp/>} />
       <Route path="/signin" element={ authUser? <ReviewerCode /> :<SingIn/>} />
-      <Route path="/signup" element={ authUser? <ReviewerCode /> :<SingUp/>} />
-      <Route path="/contact" element={ authUser? <Contact /> :<SingUp/>} />
+      <Route path="/signup" element={ authUser? <ReviewerCode /> :<SignUp/>} />
+      <Route path="/contact" element={ authUser? <Contact /> :<SignUp/>} />
 
     
     </Routes>
